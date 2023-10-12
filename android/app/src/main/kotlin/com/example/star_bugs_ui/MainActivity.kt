862a6233dd2cbd3bf5736a8d5836cdba8ec3590f
areeb786 <areebrockzz786@gmail.com>
2023-10-13 00:47:32 +0530

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/star_bugs_ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.star_bugs_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/ui/splash/screen/splash.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Splash extends StatelessWidget {
+  const Splash({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                child: Image.asset('asset/images/app_image.png'),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Text(
+                "StarBucks",
+                style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
