4d490cdd90a2982b76cda8b1cc8a42cdb9e2f4cc
areeb786 <areebrockzz786@gmail.com>
2023-10-13 02:03:32 +0530

    added splash screen

--- lib/ui/splash/screen/splash.dart
+++ lib/ui/splash/screen/splash.dart
@@ -1,11 +1,22 @@
+import 'dart:async';
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:star_bugs_ui/ui/splash/home.dart';
 
 class Splash extends StatelessWidget {
   const Splash({super.key});
 
   @override
   Widget build(BuildContext context) {
+    /*
+    * Timer for splash screen
+    * */
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(
+            context, MaterialPageRoute(builder: (context) => const Home())));
+
     return MaterialApp(
       home: Scaffold(
         body: Center(
