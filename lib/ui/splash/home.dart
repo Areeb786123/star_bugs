9eedbcc205f98b90c21d81ee8ad19cb92823f208
areeb786 <areebrockzz786@gmail.com>
2023-10-13 03:35:40 +0530

    added static data

--- lib/ui/splash/home.dart
+++ lib/ui/splash/home.dart
@@ -18,16 +18,106 @@ class Home extends StatelessWidget {
         body: SingleChildScrollView(
           child: Column(
             children: [
+              /**
+               * Banner card
+               *
+               */
               Card(
                 child: Padding(
                   padding:
-                      const EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+                      const EdgeInsets.symmetric(horizontal: 0, vertical: 0),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(12),
                     child: Image.asset('asset/images/home_coffee.png'),
                   ),
                 ),
               ),
+
+              /**
+               * card Balance
+               * */
+              Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.orange, width: 2),
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Card(
+                        child: SafeArea(
+                      child: Column(
+                        children: <Widget>[
+                          const Padding(
+                            padding: EdgeInsets.symmetric(
+                                horizontal: 10, vertical: 14),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              children: <Widget>[
+                                Text(
+                                  "Card Balance",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.normal,
+                                      color: Colors.black,
+                                      fontSize: 18),
+                                ),
+                                Text(
+                                  "rs190.00",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      color: Colors.black,
+                                      fontSize: 18),
+                                )
+                              ],
+                            ),
+                          ),
+                          const Padding(
+                              padding: EdgeInsets.symmetric(
+                                  horizontal: 20, vertical: 14),
+                              child: Divider(
+                                height: 1,
+                                color: Colors.black,
+                              )),
+                          const SizedBox(
+                            height: 4,
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: <Widget>[
+                              Image.asset(
+                                "asset/images/app_image.png",
+                                width: 80,
+                                height: 80,
+                              ),
+                              Image.asset(
+                                "asset/images/app_image.png",
+                                width: 80,
+                                height: 80,
+                              ),
+                              Image.asset(
+                                "asset/images/app_image.png",
+                                width: 80,
+                                height: 80,
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    )),
+                  )),
+              const SizedBox(height: 14),
+              Container(
+                alignment: Alignment.centerLeft,
+                child: const Padding(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Text(
+                    "Explore",
+                    style: TextStyle(fontSize: 20, color: Colors.black),
+                  ),
+                ),
+              )
+              /*
+              * list of items
+              * */
             ],
           ),
         ),
