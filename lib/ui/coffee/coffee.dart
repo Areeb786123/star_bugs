be0a2f98c5e3c75dd58cc3128b2001c9f95c2c23
areeb786 <areebrockzz786@gmail.com>
2023-10-17 00:52:34 +0530

    added app bar in coffee

--- lib/ui/coffee/coffee.dart
+++ lib/ui/coffee/coffee.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:star_bugs_ui/ui/common/base.dart';
 
+import '../../data/models/response/posts.dart';
 import '../home/home.dart';
 
 class Coffee extends StatelessWidget {
@@ -8,18 +10,31 @@ class Coffee extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: GestureDetector(
-        onTap: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => const Home()));
-        },
-        child: Container(
-          alignment: Alignment.center,
-          color: Colors.blue,
-          child: Text("hello my name is areeb"),
+    return Scaffold(
+      appBar: AppBar(
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => const Home()));
+          },
+          child: const Icon(Icons.arrow_back),
         ),
       ),
+      body: MaterialApp(
+        home: Base.emptyData(),
+      ),
+    );
+  }
+
+  Widget getData(List<Post> post) {
+    return SizedBox(
+      height: 200,
+      child: ListView.builder(
+          scrollDirection: Axis.vertical,
+          itemCount: post.length,
+          itemBuilder: (context, index) {
+            return Card(child: Text(post[index].title.toString()));
+          }),
     );
   }
 }
